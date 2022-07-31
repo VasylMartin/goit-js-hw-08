9b95a2811b67f55588f7bc21658a377c7267adab
VasylMartin <vasiliymartiniouk@gmail.com>
2022-07-31 19:54:37 +0300

    Task 1 done

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,25 @@
-// Add imports above this line
-import { galleryItems } from './gallery-items';
+import { galleryItems } from "./gallery-items.js";
+import "simplelightbox/dist/simple-lightbox.min.css";
+import SimpleLightbox from "simplelightbox";
 // Change code below this line
 
-console.log(galleryItems);
+function createGallery(array) {
+  return array.reduce(
+    (acc, item) =>
+      acc +
+      `<a class="gallery__item" href="${item.original}">
+  <img class="gallery__image" src="${item.preview}" alt="${item.description}" />
+</a>`,
+    ""
+  );
+}
+const result = createGallery(galleryItems);
+
+const gallery = document.querySelector(".gallery");
+gallery.insertAdjacentHTML("beforeend", result);
+
+var lightbox = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+  captionPosition: "bottom",
+});
