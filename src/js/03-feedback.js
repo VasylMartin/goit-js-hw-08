ec2b41a00f571a8c643ac779dd39fb2eee646ba9
VasylMartin <vasiliymartiniouk@gmail.com>
2022-08-02 20:44:59 +0300

    fix Task3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -10,9 +10,9 @@ form.addEventListener('submit', onFormSubmit)
 
 updForm()
 
-function onInput(event) {
-    dataForm.email = event.currentTarget.email.value
-    dataForm.message = event.currentTarget.message.value
+function onInput() {
+    dataForm.email = form.elements.email.value
+    dataForm.message = form.elements.message.value
     localStorage.setItem(STORAGE_KEY, JSON.stringify(dataForm))
 }
 
@@ -35,5 +35,35 @@ function updForm() {
     }
 }
 
-console.log(dataForm)
+// import throttle from 'lodash.throttle';
 
+// const formEl = document.querySelector(".feedback-form");
+// const inputEl = document.querySelector(".feedback-form input");
+// const textareaEl = document.querySelector(".feedback-form textarea");
+
+// const LOCAL_KEY = "feedback-form-state";
+
+// const formData = localStorage.getItem(LOCAL_KEY) ? JSON.parse(localStorage.getItem(LOCAL_KEY)):{};
+
+// const saveItem = localStorage.getItem(LOCAL_KEY);
+//         if (saveItem) {
+//             const itemParse = JSON.parse(saveItem);
+//             inputEl.value = itemParse.email || "";
+//             textareaEl.value = itemParse.message || "";
+//         }
+//         else {
+//             inputEl.value = "";
+//             textareaEl.value = "";
+//         }
+// formEl.addEventListener("input", throttle(handleSaveInputValue, 500));
+// formEl.addEventListener('submit', handleSubmitButtonClick);
+// function handleSaveInputValue(event) {
+//     formData[event.target.name] = event.target.value;
+//     localStorage.setItem(LOCAL_KEY, JSON.stringify(formData));
+// }
+// function handleSubmitButtonClick(event) {
+//     event.preventDefault();
+//     console.log(JSON.parse(localStorage.getItem(LOCAL_KEY)));
+//     localStorage.removeItem(LOCAL_KEY);
+//     event.currentTarget.reset();
+// }
